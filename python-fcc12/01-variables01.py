bcaf1c7685879daca503866c3de68fcaa3c08071
pabloqpacin-GL76 <pabloqpacin@zohomail.eu>
2022-10-14 16:57:27 +0200

    getting python up to date

---
+++ python-fcc12/01-variables01.py
@@ -0,0 +1,47 @@
+
+
+"""
+Lesson: use variables and user input for string concatenation:
+EXAMPLE - different ways to print a given variable
+MADLIBS GAME - prints a script upon user input
+Run the file in Terminal to play.
+"""
+
+# EXAMPLE
+print("    EXAMPLE: see some lines using the 'mateys' variable")
+
+# Set a variable
+supdawg = "mateys"
+
+# Different ways to print the variable
+print("awe thanks " + supdawg + ", now that's it")
+print("awe Gawd {}".format(supdawg) + ", this is fun")
+print(f"yo {supdawg}, what's going on")
+
+
+# TEST-CONTINUE
+input("\n\
+Continue? (press enter) \n\
+")
+
+
+
+# MADLIBS GAME
+print("    MADLIBS GAME: now you set the variables!")
+
+# Prepare the variables for user input
+who = input("Who: ")
+when = input("When: ")
+where = input("Where: ")
+act1 = input("Activity 1: ")
+act2 = input("Activity 2: ")
+
+
+# Script the madlibs
+madlibs = f"    Sup dawg! Heard of {who}? Crazy shite aye!\n\
+    So {when} {where}... Unbelievable.\n\
+    Now {who} should either {act1} or {act2}, whaeva.\n\
+    Well see you later Maricarmen 💃"
+
+# And we print the script integrating the variables upon user input
+print(madlibs)
--- python-fcc12/README.md
+++ python-fcc12/README.md
@@ -1,1 +1,18 @@
-# fCC_12_beginner_projects
+# Free Code Camp - 12 Beginner Python Projects
+
+[Video tutorial](https://youtu.be/8ext9G7xspg) by [@kylieyying](https://github.com/kying18) 
+## Introduction
+
+Today is Friday the 14th October 2022. However it was in July when I undertook this FCC12 project. I had never coded before, I didn't know what the terminal was and I was so confused when I heard of "python interpreter". Awe, me first steps 😅.
+
+Well yes I uplodaded the project files to GitHub right after finishing the tutorial because sharing is caring... Butv tbf I didn't even use **git** back then and the project files are likely to be a pretty mess atm. So it is time to go through the video and the code again commenting and explaining whatever is happening in each file. Let's-a-go!
+
+
+## How to run the code
+
+In a nushell, you need **python** and we are using **VSCode** to write our code, which includes terminal
+integration for us to run and see our code.
+
+## 01 - Madlibs
+
+Change the MadLibs script and improve general commenting for clarity.
